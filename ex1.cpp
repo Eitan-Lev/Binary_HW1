c02308b54a16079f14b97b03781b0cc70ca2f6af
royhirsch1 <38239587+royhirsch1@users.noreply.github.com>
2018-04-13 18:48:44 +0300

    working version based on proccount.cpp
    remaining tasks before submission: 
    
    *check Image address (IMG_ENTRY) differences
    *check image name format ('/')
    *verify that 3 sec runtime limit is user time
    *compile using t2

--- ex1.cpp
+++ ex1.cpp
@@ -1,185 +1,173 @@
-#include "pin.H"
-#include <iostream>
+
+//
+// This tool counts the number of times a routine is executed and 
+// the number of instructions executed in a routine
+//
+
 #include <fstream>
-#include <map>
-#include <string>
+#include <iomanip>
+#include <iostream>
+#include <string.h>
+#include "pin.H"
 
-//#include <tuple>
+ofstream outFile;
 
-using namespace::std;
+// Holds instruction count for a single procedure
+typedef struct RtnCount
+{
+    string _name;
+    string _image;
+    ADDRINT _address;
+	ADDRINT _image_address;
+    RTN _rtn;
+//    UINT64 _rtnCount;
+    UINT64 _icount;
+    struct RtnCount * _next;
+} RTN_COUNT;
+
+// Linked list of instruction counts for each routine
+RTN_COUNT * RtnList = 0;
+
+// This function is called before every instruction is executed
+VOID docount(UINT64 * counter)
+{
+    (*counter)++;
+}
+    
+const char * StripPath(const char * path)
+{
+    const char * file = strrchr(path,'/');
+    if (file)
+        return file+1;
+    else
+        return path;
+}
+
+// Pin calls this function every time a new rtn is executed
+VOID Routine(RTN rtn, VOID *v)
+{
+    
+    // Allocate a counter for this routine
+    RTN_COUNT * rc = new RTN_COUNT;
+
+    // The RTN goes away when the image is unloaded, so save it now
+    // because we need it in the fini
+    rc->_name = RTN_Name(rtn);
+    rc->_image = StripPath(IMG_Name(SEC_Img(RTN_Sec(rtn))).c_str());
+    rc->_address = RTN_Address(rtn);
+	rc->_image_address = IMG_Entry(SEC_Img(RTN_Sec(rtn)));
+    rc->_icount = 0;
+    //rc->_rtnCount = 0;
+
+    // Add to list of routines
+    rc->_next = RtnList;
+    RtnList = rc;
+            
+    RTN_Open(rtn);
+            
+//    // Insert a call at the entry point of a routine to increment the call count
+//    RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)docount, IARG_PTR, &(rc->_rtnCount), IARG_END);
+    
+    // For each instruction of the routine
+    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
+    {
+        // Insert a call to docount to increment the instruction counter for this rtn
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_PTR, &(rc->_icount), IARG_END);
+    }
+
+    
+    RTN_Close(rtn);
+}
+
+
+RTN_COUNT* popMaxRoutine(){
+	RTN_COUNT *  head = RtnList;
+	//INPUT CHECK: EMPTY LIST OR LAST ELEMENT
+	if(!head)
+		return 0;
+	if(!head->_next){
+		RtnList=0;
+		return head;
+	}
+	//MAX COUNT INIT
+	RTN_COUNT *  rc = RtnList; // list iterator
+	UINT64 max_count = rc->_icount;
+	RTN_COUNT* max_routine = rc;
+	//MAX COUNT SEARCH
+	RTN_COUNT * prev = 0; // pointer to the prev node of the maximal element
+	while(rc->_next){
+		//if next element is bigger, update state
+		UINT64 cur_count = rc->_next->_icount;
+		 if ( cur_count > max_count){
+            max_count = cur_count;
+            max_routine = rc->_next;
+            prev = rc;
+        }
+		rc = rc->_next;
+	}
+	if(prev){ 
+		prev->_next = max_routine->_next;
+	}else{ //first element is maximal
+		RtnList = RtnList->_next;
+	}
+	
+	return max_routine;
+	
+}
+
+
+// This function is called when the application exits
+// It prints the name and count for each procedure
+VOID Fini(INT32 code, VOID *v)
+{
+    while (RtnList){
+        RTN_COUNT* currentRoutine = popMaxRoutine();
+
+        if (currentRoutine ->_icount > 0) {
+			 outFile << "0x"<< hex << currentRoutine->_image_address << dec <<  "," << currentRoutine->_image <<  ","
+             << "0x" << hex << currentRoutine->_address << dec << "," << currentRoutine->_name << "," << currentRoutine->_icount << endl;
+		}
+
+	}
+
+}
 
 /* ===================================================================== */
-/* Global Variables                                                      */
+/* Print Help Message                                                    */
 /* ===================================================================== */
 
-//Using 2 map objects, for name and counts
-map <UINT32, UINT32> RTNTABLE;
-map <UINT32, string> RTNNAMES;
-
-class RoutineData {
-public:
-  /*RTN Routine;
-  IMG Img;*/
-  UINT32 Count;
-  ADDRINT _RTN_ADDRESS;
-  string _RTN_NAME;
-  ADDRINT _IMG_ADDRESS;
-  //string _IMG_NAME;
-  /*RoutineData(RTN rtn, IMG img)
-    : Routine(rtn), Img(img), Count(0) {}*/
-  RoutineData(ADDRINT rtnAddress,string rtnName, ADDRINT imgAddress)
-    : Count(0), _RTN_ADDRESS(rtnAddress), _RTN_NAME(rtnName),
-      _IMG_ADDRESS(imgAddress) {}
-};
-
-//RoutineData rtnData;
-map <UINT32, RoutineData> RoutineMap;
-
-/*map <UINT32, UINT32> countMap;
-map <UINT32, ADDRINT> rtnAddresses;
-map <UINT32, string> rtnNames;
-map <UINT32, ADDRINT> imgAdresses;
-map <UINT32, string> imgNames;
-map <UINT32, const string&> imgNames2;*/
-
-
-
-struct RoutineData2 {
-  RTN Routine;
-  IMG Img;
-  UINT32 Count;
-  //map <UINT32, RTN> _RTN_MAP;
-};
-
-
-/* ===================================================================== */
-/* Help Message                                                          */
-/* ===================================================================== */
 INT32 Usage()
 {
-    cerr <<
-        "This tool prints out the number of dynamic instructions executed to stderr"
-		<< endl << KNOB_BASE::StringKnobSummary() << endl;
-
+    cerr << "This Pintool counts the number of times a routine is executed" << endl;
+    cerr << "and the number of instructions executed in a routine" << endl;
+    cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
     return -1;
 }
 
-/* ===================================================================== */
 
-//VOID PIN_FAST_ANALYSIS_CALL docount(UINT32 count, UINT32 RoutineID) { RTNTABLE[RoutineID] += count; }
-VOID docount(UINT32 count, UINT32 RoutineID) { ((RoutineMap.find(RoutineID))->second).Count += count; }
-
-/* ===================================================================== */
-
-VOID BasicBlock(TRACE trc , VOID *v)
-{
-	//BBL head
-	for (BBL bbl =TRACE_BblHead(trc); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-	{
-		RTN CurrentRTN = RTN_FindByAddress(BBL_Address(bbl));
-		UINT32 CurrentId = RTN_Id(CurrentRTN);
-
-    ADDRINT RoutineAddress = RTN_Address(CurrentRTN);
-    string RoutineName = RTN_FindNameByAddress(BBL_Address(bbl));
-
-    IMG CurrentImg = IMG_FindByAddress(BBL_Address(bbl));
-
-    //const string& tmp = IMG_Name(CurrentImg);
-    //cout << "tmp: " << &tmp << endl;
-    /*string ImgName;
-    for (unsigned int i = 0; i < tmp.length(); i++) {
-      ImgName.insert(*(tmp + i);
-    }*/
-    //string ImgName(tmp);
-    //strcpy(&tmp, ImgName);
-    //strcpy(IMG_Name(CurrentImg), ImgName);
-
-    ADDRINT ImgAddress = IMG_Entry(CurrentImg);
-
-    //if (countMap.find(CurrentId) == countMap.end()) {
-    if (RoutineMap.find(CurrentId) == RoutineMap.end()) {
-        RoutineData rtnData(RoutineAddress, RoutineName, ImgAddress);
-        RoutineMap.insert(pair<UINT32,RoutineData>(CurrentId,rtnData));
-
-        //RoutineData rtnData(CurrentRTN, CurrentImg);
-        //RoutineMap.insert(pair<UINT32,RoutineData>(CurrentId,rtnData));
-
-        /*countMap.insert(pair<UINT32,UINT32>(CurrentId,0));
-        rtnAddresses.insert(pair<UINT32,ADDRINT>(CurrentId,RoutineAddress));
-        rtnNames.insert(pair<UINT32,string>(CurrentId,RoutineName));
-        imgAdresses.insert(pair<UINT32,ADDRINT>(CurrentId,ImgAddress));*/
-        //imgNames.insert(pair<UINT32,string>(CurrentId,ImgName));
-        //imgNames2.insert(pair<UINT32,const string&>(CurrentId,tmp));
-
-        RTNTABLE.insert(pair<UINT32,UINT32>(CurrentId,0));  //create new one
-    }
-		//if(RTNTABLE.find(CurrentId) == RTNTABLE.end()) //not exist
-			//RTNTABLE.insert(pair<UINT32,UINT32>(CurrentId,0));  //create new one
-
-		// check is the element already exist in RTNNAMES map
-		if(RTNNAMES.find(CurrentId) == RTNNAMES.end()) //not exist
-			RTNNAMES.insert(pair<UINT32,string>(CurrentId,RTN_FindNameByAddress(BBL_Address(bbl))));  	//create new one
-
-		BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)docount, IARG_UINT32, BBL_NumIns(bbl), IARG_UINT32, CurrentRTN, IARG_END);
-	}
-}
-
-/* ===================================================================== */
-
-VOID Fini(INT32 code, VOID *v)
-{
-	//open file
-	ofstream oFile;
-	oFile.open("rtn-output.csv");
-  //map <UINT32,UINT32> MaxMap;
-  map <UINT32,RoutineData> MaxMap;
-  //oFile << '"' << it_CurrentName->second << '"' << " icount " << it_CurrentNumber->second << endl;
-  for (map<UINT32,RoutineData>::iterator iter = RoutineMap.begin(); iter != RoutineMap.end(); ++iter) {
-    UINT32 Count = (iter->second).Count;
-    MaxMap.insert(pair<UINT32,RoutineData>(Count,iter->second));
-  }
-  for (map<UINT32,RoutineData>::reverse_iterator iter = MaxMap.rbegin(); iter != MaxMap.rend(); ++iter) {
-    /*RTN Routine((iter->second).Routine);
-    IMG Img((iter->second).Img);
-    ADDRINT ImgAddress = IMG_Entry(Img);
-    //string ImgName = IMG_Name(Img);
-    ADDRINT RoutineAddress = RTN_Address(Routine);
-    string RoutineName = RTN_Name(Routine);*/
-
-    ADDRINT imgAddress = (iter->second)._IMG_ADDRESS;
-    string ImgAddress = StringFromAddrint(imgAddress);
-    ADDRINT routineAddress = (iter->second)._RTN_ADDRESS;
-    string RoutineAddress = StringFromAddrint(routineAddress);
-    string RoutineName = (iter->second)._RTN_NAME;
-    UINT32 Count = iter->first;
-    oFile << ImgAddress << "," << "," << RoutineAddress << ","
-    << RoutineName << "," << Count << endl;
-  }
-	oFile.close(); //close file
-}
 
 /* ===================================================================== */
 /* Main                                                                  */
 /* ===================================================================== */
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-	// Initialize pin and pin symbol table code
-	PIN_InitSymbols();
-	// Initialize pin
-	if( PIN_Init(argc,argv) )
-		return Usage();
+    // Initialize symbol table code, needed for rtn instrumentation
+    PIN_InitSymbols();
 
-	TRACE_AddInstrumentFunction(BasicBlock, 0);
-	// Register Fini to be called when the application exits
-	PIN_AddFiniFunction(Fini, 0);
-	// Start the program, never returns
-	PIN_StartProgram();
+    outFile.open("rtn-output.csv");
 
+    // Initialize pin
+    if (PIN_Init(argc, argv)) return Usage();
+
+    // Register Routine to be called to instrument rtn
+    RTN_AddInstrumentFunction(Routine, 0);
+
+    // Register Fini to be called when the application exits
+    PIN_AddFiniFunction(Fini, 0);
+    
+    // Start the program, never returns
+    PIN_StartProgram();
+    
     return 0;
 }
-
-/* ===================================================================== */
-/* eof */
-/* ===================================================================== */
-
